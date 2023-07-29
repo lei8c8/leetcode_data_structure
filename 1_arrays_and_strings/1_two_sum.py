69108498916b825c7ae2c8bc0e2c3f4e70886811
tony <tony@Leis-MBP.fios-router.home>
2023-07-29 16:18:06 -0400

    finished 1_two_sum.py

---
+++ 1_arrays_and_strings/1_two_sum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx_map = {}
+
+        for i, v in enumerate(nums):
+            if target - v in idx_map:
+                return [idx_map[target - v], i]
+            idx_map[v] = i
+        
