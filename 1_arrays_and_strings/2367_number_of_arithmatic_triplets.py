4f235d87a0af951d49a8a6cba25fdeaba8842c5a
tony <tony@Leis-MBP.fios-router.home>
2023-07-30 18:07:19 -0400

    solved 2367_number_of_arithmatic_triplets.py

---
+++ 1_arrays_and_strings/2367_number_of_arithmatic_triplets.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        seen = set()
+        ans = 0
+
+        for num in nums:
+            if (num - diff) in seen and (num - 2 * diff) in seen:
+                ans += 1
+            seen.add(num)
+
+        return ans
