523f74f33e0d4c7cd5c91601348315429eb7747a
tony <tony@Leis-MBP.fios-router.home>
2023-07-30 21:29:01 -0400

    solved 1237_find_positive_int_solutin.py

---
+++ 1_arrays_and_strings/1237_find_positive_int_solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class CustomFunction:
+       def f(self, x, y):
+        pass
+  
+
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        ans = []
+        y = 1000
+        for x in range(1, 1001):
+            while y > 1 and customfunction.f(x, y) > z:
+                y -= 1
+            if customfunction.f(x, y) == z:
+                ans.append([x, y])
+        return ans
+        
