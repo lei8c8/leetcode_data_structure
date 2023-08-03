c58522563acc50471a30527b306f4dff67115b6b
tony <tony@Leis-MBP.fios-router.home>
2023-08-02 21:44:42 -0400

    solved 795 with two methods

--- 1_arrays_and_strings/795_number_of_subarrays_with_bounded_max.py
+++ 1_arrays_and_strings/795_number_of_subarrays_with_bounded_max.py
@@ -11,6 +11,22 @@ class Solution:
         return cap(right) - cap(left - 1)
 
 
+class Solution2:
+    def numSubarrayBoundedMax(self, nums, left, right):
+        n = len(nums)
+        dp = [0] * n
+        prev= -1
+        for i in range(n):
+            if nums[i] < left and i > 0:
+                dp[i] = dp[i - 1]
+            elif nums[i] > right:
+                dp[i] = 0 
+                prev = i
+            elif left <= nums[i] <= right:
+                dp[i] = i - prev
+        return sum(dp)
+
+
 if __name__ == '__main__':
     solution = Solution()
     data = [73,55,36,5,55,14,9,7,72,52]
